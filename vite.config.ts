d0df89987be6901f45b13b8e76c44435475f5079
Netolink <netolink.ltd@gmail.com>
2026-09-13 13:01:58 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_ACTIONS === 'true' ? '/Google-Algorithm-Tracking-and-Volatility/' : '/',
+    base: process.env.GITHUB_ACTIONS === 'true' ? '/google-algorithm-tracking-and-volatility/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
